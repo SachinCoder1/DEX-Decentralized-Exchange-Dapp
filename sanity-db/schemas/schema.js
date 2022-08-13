889072f900f84e64dc88cc0fc02fdacbbd41021d
SachinCoder1 <sachinhlo232@gmail.com>
2022-08-13 13:39:53 +0530

    transaction types (sanity)

--- sanity-db/schemas/schema.js
+++ sanity-db/schemas/schema.js
@@ -1,10 +1,13 @@
 import createSchema from 'part:@sanity/base/schema-creator'
-
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+import { userTypes } from './userTypes'
+import { transactionTypes } from './transactionTypes'
 
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
-    /* Your types here! */
+    userTypes, transactionTypes
+
   ]),
 })
