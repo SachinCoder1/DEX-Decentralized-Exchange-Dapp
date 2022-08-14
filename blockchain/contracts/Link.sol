804ff7640e015f8511ca95ef0d3c89b19a545a92
SachinCoder1 <sachinhlo232@gmail.com>
2022-08-14 09:13:25 +0530

    Dai, Link, Usdc contracts

---
+++ blockchain/contracts/Link.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Link is ERC20, Ownable {
+
+
+    /* Constructor */
+    constructor() ERC20("Link", "LINK"){}
+
+    /* Logics */
+
+    function mint(address _to, uint256 _amount) external payable {
+        _mint(_to, _amount);
+    }
+
+    receive() external payable {
+        
+    }
+
+
+}
